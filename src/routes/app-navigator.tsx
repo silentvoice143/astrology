8bb897aff57d655d6902624584ddd6ede1a0e2ef
silentvoice143 <silentvoice4143@gmail.com>
2025-06-14 18:00:03 +0530

    fix login flow

--- src/routes/app-navigator.tsx
+++ src/routes/app-navigator.tsx
@@ -6,28 +6,40 @@ import PrivateRoutes from './private-route';
 import {useAppDispatch, useAppSelector} from '../hooks/redux-hook';
 import {userDetail} from '../store/reducer/user';
 import {logout} from '../store/reducer/auth';
-import {View} from 'react-native-reanimated/lib/typescript/Animated';
-import {Text} from 'react-native';
+
+import {Text, View} from 'react-native';
 
 export default function AppNavigator() {
   const {token} = useAppSelector((state: any) => state.auth);
-  const [loading, setLoading] = useState(true);
-  const isAuthenticated = !!token;
   const dispatch = useAppDispatch();
+  const [loading, setLoading] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  const checkAuth = async () => {
-    try {
-      const {payload} = await dispatch(userDetail());
-      if (!payload?.success) {
-        dispatch(logout());
+  useEffect(() => {
+    const checkAuth = async () => {
+      if (token) {
+        try {
+          const {payload} = await dispatch(userDetail());
+          if (payload?.success) {
+            setIsAuthenticated(true);
+          } else {
+            dispatch(logout());
+            setIsAuthenticated(false);
+          }
+        } catch (err) {
+          console.log(err);
+          dispatch(logout());
+          setIsAuthenticated(false);
+        }
+      } else {
+        setIsAuthenticated(false);
       }
-    } catch (err) {
-      console.log(err);
-      dispatch(logout());
-    } finally {
+
       setLoading(false);
-    }
-  };
+    };
+
+    checkAuth();
+  }, [token, dispatch]);
 
   if (loading) {
     return (
@@ -36,12 +48,6 @@ export default function AppNavigator() {
       </View>
     );
   }
-
-  useEffect(() => {
-    if (!!token) {
-      checkAuth();
-    }
-  }, [token]);
 
   return (
     <NavigationContainer>
